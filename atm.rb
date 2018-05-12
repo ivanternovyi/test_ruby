02983ba3da3b6161fc111b621f26d45dbd0f26d4
ivanternovyi <ivanternovyi@gmail.com>
2018-05-12 19:27:52 +0300

    Added project

---
+++ atm.rb
@@ -0,0 +1,116 @@
+require 'yaml'
+require 'ostruct'
+require_relative 'constants'
+
+class ATM
+  def initialize
+    @data = YAML.load_file('config.yml')
+    @user = welcome_menu
+    choice_menu
+  end
+
+  private
+
+  def find_user(id, passw)
+    @data['accounts'].each_pair do |id_key, info|
+      if id == id_key.to_s && info['password'] == passw
+        return OpenStruct.new(name: info['name'], password: info['password'],
+                              balance: info['balance'])
+      end
+    end
+    nil
+  end
+
+  def atm_balance
+    @data['banknotes'].map { |cash, count| cash * count }.reduce(:+)
+  end
+
+  def can_cash_in?(amount)
+    @data['banknotes'].each_pair do |cash, count|
+      count.times do
+        break unless amount - cash >= 0
+        amount -= cash
+        count -= 1
+      end
+    end
+    amount.zero?
+  end
+
+  def log_out
+    puts "\n#{@user[:name]}, " + GOOD_BYE
+    @user = welcome_menu
+    puts "\nHello, #{@user[:name]}!\n\n"
+  end
+
+  def check_withdraw(amount)
+    if amount > @user[:balance]
+      print INSUFFICIENT_FUNDS
+    elsif amount > atm_balance
+      print MAX_AMOUNT_ATM + '₴' + atm_balance.to_s + ENTER_DIFF_VALUE
+    elsif !can_cash_in?(amount)
+      print COMPOSE_ERR
+    else
+      true
+    end
+  end
+
+  def withdraw(amount)
+    @user[:balance] -= amount
+    @data['accounts'].each_pair { |_id, info| info['balance'] -= amount if info['name'] == @user['name']}
+    @data['banknotes'].each_pair do |cash, count|
+      count.times do
+        break unless amount - cash >= 0
+        amount -= cash
+        @data['banknotes'][cash] -= 1
+      end
+    end
+  end
+
+  def welcome_menu
+    loop do
+      print ENTER_ACC_ID
+      id = gets.chomp
+      print ENTER_PASSW
+      passw = gets.chomp
+      return find_user(id, passw) if find_user(id, passw)
+      puts ERR_LOGIN
+    end
+  end
+
+  def choice_menu
+    puts "\nHello, #{@user[:name]}!\n\n"
+    loop do
+      print CHOICES
+      choice = gets.chomp
+      case choice
+      when '1'
+        puts "\nYour Current Balance is ₴#{@user[:balance]}\n\n"
+      when '2'
+        withdraw_menu
+      when '3'
+        log_out
+      else
+        print BAD_CHOICE
+      end
+    end
+  end
+
+  def withdraw_menu
+    print WISH_WITHDRAW
+    loop do
+      amount = gets.chomp
+      if amount.to_i.to_s == amount
+        amount = amount.to_i
+      else
+        print ERR_INT
+        next
+      end
+      next unless check_withdraw amount
+      withdraw amount
+      print "\nYour New Balance is ₴#{@user[:balance]}\n\n"
+      break
+    end
+  end
+end
+
+ATM.new
---
+++ constants.rb
@@ -0,0 +1,14 @@
+INSUFFICIENT_FUNDS = "\nERROR: INSUFFICIENT FUNDS!! PLEASE ENTER A DIFFERENT AMOUNT:\n> ".freeze
+MAX_AMOUNT_ATM = "\nERROR: THE MAXIMUM AMOUNT AVAILABLE IN THIS ATM IS ".freeze
+ENTER_DIFF_VALUE = " PLEASE ENTER A DIFFERENT AMOUNT:\n> ".freeze
+COMPOSE_ERR = "\nERROR: THE AMOUNT YOU REQUESTED CANNOT BE COMPOSED FROM BILLS"\
+                  " AVAILABLE IN THIS ATM. PLEASE ENTER A DIFFERENT AMOUNT:\n> ".freeze
+ERR_INT = "\nERROR: SHOULD BE INTEGER, PLEASE RETRY:\n> ".freeze
+WISH_WITHDRAW = "\nEnter Amount You Wish to Withdraw:\n> ".freeze
+ERR_LOGIN = "\nERROR: ACCOUNT NUMBER AND PASSWORD DON'T MATCH\n\n".freeze
+CHOICES = "Please Choose From the Following Options:\n1. Display Balance\n" \
+                "2. Withdraw\n3. Log Out\n> ".freeze
+BAD_CHOICE = "\nERROR: BAD COMMAND, PLEASE RETRY\n\n".freeze
+ENTER_ACC_ID = "Please Enter Your Account Number:\n> ".freeze
+ENTER_PASSW = "Enter Your Password:\n> ".freeze
+GOOD_BYE = "Thank You For Using Our ATM. Good-Bye!\n\n".freeze
